5a1e8e0a36566f75d8522bfc39afd31817c0aa97
yuanhang <295043131@qq.com>
2024-06-14 22:04:11 +0800

    表单校验Vuelidate

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -6,26 +6,38 @@
       <template #label>
         <span>*</span> 用户名
       </template>
-      <el-input v-model="form.username" />
+      <el-input v-model="form.username" @blur="v$.username.$touch" />
+      <div class="error" >{{ v$.username.$errors[0]?.$message }}</div>
     </el-form-item>
     <el-form-item>
       <template #label>
         <span>*</span> 密码
       </template>
-      <el-input v-model="form.password" />
+      <el-input v-model="form.password" @blur="v$.password.$touch"/>
+      <div class="error">{{ v$.password.$errors[0]?.$message }}</div>
     </el-form-item>
-    <el-button type="primary" @click="onSubmit">登录</el-button>
+    <el-button type="primary" @click="onSubmit" :disabled="v$.$invalid">登录</el-button>
   </el-form>
 
 </template>
 
 <script setup>
+
+import { useVuelidate } from '@vuelidate/core'
+import { required,helpers } from '@vuelidate/validators'
 import AuthAPI from "@/api/auth.js";
 
 const form = reactive({
   username: '',
   password: ''
 });
+
+const rules = {
+  username: { required: helpers.withMessage('用户名不能为空', required)},
+  password: { required: helpers.withMessage('密码不能为空', required) }
+}
+
+const v$ = useVuelidate(rules, form)
 
 
 function handleLoginData() {
@@ -40,11 +52,14 @@ function onSubmit() {
         ElMessage.error(error || "系统出错");
       })
 }
+
 </script>
 
 <style lang="less" scoped>
 
-
+.error {
+  color: @error-color;
+}
 .el-form-item span {
   color: @error-color;
 }
