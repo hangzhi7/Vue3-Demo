39b3f5015cf7906e538e174bac178c41cd9142e5
yuanhang <295043131@qq.com>
2024-06-14 21:28:45 +0800

    整合ElementPlus

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -19,22 +19,11 @@ const count = ref(0)
     </p>
   </div>
 
-  <p>
-    Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
-  </p>
-  <p>
-    Install
-    <a href="https://github.com/vuejs/language-tools" target="_blank">Volar</a>
-    in your IDE for a better DX
-  </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
+  <div>
+    <el-button type="success">Success</el-button>
+    <el-button type="info">Info</el-button>
+    <el-button type="warning">Warning</el-button>
+    <el-button type="danger">Danger</el-button>
+  </div>
 </template>
 
-<style scoped>
-.read-the-docs {
-  color: #888;
-}
-</style>
--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ const pathSrc = resolve(__dirname, "src");
 
 import AutoImport from "unplugin-auto-import/vite"
 import Components from "unplugin-vue-components/vite";
+import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -15,15 +16,24 @@ export default defineConfig({
     AutoImport({
       // 自动导入 Vue 相关函数，如：ref, reactive, toRef 等
       imports: ["vue", "vue-router"],
+      resolvers: [
+        // 自动导入 Element Plus 相关函数，如：ElMessage, ElMessageBox... (带样式)
+        ElementPlusResolver(),
+      ],
       // 指定自动导入函数TS类型声明文件路径
       // 也可以不生成声明文件 dts: false
       dts: resolve(pathSrc,"types","auto-imports.d.ts"),
     }),
     Components({
+      resolvers: [
+        // 自动导入 Element Plus 组件
+        ElementPlusResolver(),
+      ],
       // 指定自动导入组件TS类型声明文件路径
       // 也可以不生成声明文件 dts: false
       dts: resolve(pathSrc,"types","components.d.ts"),
     }),
+
   ],
   resolve: {
     alias: {
